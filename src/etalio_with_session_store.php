857d76eda2f0e645eeff17d0e9a12712b7f9ea44
Martin Kurtsson <martin.kurtsson@screeninteraction.com>
2014-02-25 16:42:17 +0100

    Typo in license header

--- src/etalio_with_session_store.php
+++ src/etalio_with_session_store.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2014 LM Ericsson AB
+ * Copyright 2014 Ericsson AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
